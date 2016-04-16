62073e95982db3bf941f49b21f3d986617479509
Jadugarr <ReneLoescher@gmx.net>
2016-04-16 04:12:26 +0200

    First commit

---
+++ Scripts/Event/EventHandler.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class EventHandler
+{	
+	#region Delegates/Events
+
+
+
+	#endregion
+
+	#region Private Felder
+
+	/// <summary>
+	/// The arguments of the event.
+	/// </summary>
+	private IEvent evt_args;
+
+	#endregion
+
+	public EventHandler()
+	{
+		/*if (evt == null) 
+		{
+			throw new ArgumentNullException();
+		}
+
+		this.evt_args = evt;*/
+	}
+
+}
