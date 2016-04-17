f4b8d383d2e2bc5ec38ca4161aae59f6906ef00b
r0bzilla <r0bzilla@gmx.net>
2016-04-17 20:51:05 +0200

    Intro Try Again
    without conflicts plz

---
+++ Scripts/InstructionsManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class InstructionsManager : MonoBehaviour {
+
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetButtonDown("Fire1") || Input.GetButtonDown("Fire2") || Input.GetButtonDown("Fire3"))
+		{
+			SceneManager.LoadScene( "Title" );
+		}
+	}
+}
---
+++ Scripts/TitleManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class TitleManager : MonoBehaviour {
+
+	public GameObject ArrowUp;
+	public GameObject ArrowDown;
+
+	private bool isStartSelected = true;
+	private bool isAxisDown = false;
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetButtonDown("Fire1"))
+		{
+			SceneManager.LoadScene( isStartSelected? "Scene" : "Instructions" );
+		}
+		else if(!isAxisDown && Input.GetAxisRaw("Vertical") != 0)
+		{
+			isStartSelected = !isStartSelected;
+			ArrowUp.SetActive( isStartSelected );
+			ArrowDown.SetActive( !isStartSelected );
+		}
+		isAxisDown = Input.GetAxisRaw("Vertical") != 0;
+	}
+}
