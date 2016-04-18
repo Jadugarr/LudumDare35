9bc894d5e3730eb0538d41430c5065bd12916645
SirObvious <Pascal.Worreschk@gmail.com>
2016-04-18 23:05:53 +0200

    Wolken System

---
+++ Scripts/CloudController.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class CloudController : MonoBehaviour {
+
+    public List<Sprite> backClouds;
+    public List<Sprite> frontClouds;
+    public GameObject prefabCloud;
+
+    //
+    public float spawnTimeMin, spawnTimeMax;
+    public float cloudSpeedFrontMin, cloudSpeedFrontMax, cloudSpeedBackMin, cloudSpeedBackMax;
+    public float spawnPoint;
+
+    //
+    private float spawnAcc = 0f;
+    private float spawnTime;
+
+    // Use this for initialization
+    void Start () {
+        spawnTime = CreateSpawnTime();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        spawnAcc += Time.deltaTime;
+        if(spawnAcc > spawnTime)
+        {
+            if (Random.Range(0, 2) > 0)
+            {
+                Vector2 spawnPos = new Vector2(this.transform.position.x, GetRandomYPos());
+                GameObject cloud = (GameObject)Instantiate(prefabCloud, spawnPos, prefabCloud.transform.rotation);
+                SpriteRenderer spriteRen = cloud.GetComponent < SpriteRenderer >();
+                spriteRen.sprite = SetRandomSprite(frontClouds);
+                CloudMoving cloudMovScript = cloud.GetComponent<CloudMoving>();
+                cloudMovScript.velocity = Random.Range(cloudSpeedFrontMin, cloudSpeedFrontMax);
+            } else
+            {
+                Vector2 spawnPos = new Vector2(this.transform.position.x, GetRandomYPos());
+                GameObject cloud = (GameObject)Instantiate(prefabCloud, spawnPos, prefabCloud.transform.rotation);
+                SpriteRenderer spriteRen = cloud.GetComponent<SpriteRenderer>();
+                spriteRen.sprite = SetRandomSprite(backClouds);
+                CloudMoving cloudMovScript = cloud.GetComponent<CloudMoving>();
+                cloudMovScript.velocity = Random.Range(cloudSpeedBackMin, cloudSpeedBackMax);
+            }
+            spawnTime = CreateSpawnTime();
+            spawnAcc = 0f;
+        }
+	}
+
+    private float CreateSpawnTime()
+    {
+        return Random.Range(spawnTimeMin, spawnTimeMax);
+    }
+
+    private float GetRandomYPos()
+    {
+        return Random.Range(this.transform.position.y, this.transform.position.y + spawnPoint);
+    }
+
+    private Sprite SetRandomSprite(List<Sprite> spriteList)
+    {
+        return spriteList[Random.Range(0, spriteList.Count)];
+    }
+
+}
---
+++ Scripts/CloudMoving.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudMoving : MonoBehaviour {
+
+    public float velocity = 1f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 speed = new Vector3(-(velocity * Time.deltaTime), 0f, 0f);
+       // Debug.Log(speed);
+        this.transform.Translate(speed);
+        if(this.transform.position.x < -5f)
+        {
+            Destroy(this.gameObject);
+        }
+	}
+}
