40d8a464906bde675e2e3c377720ba539637ec65
r0bzilla <r0bzilla@gmx.net>
2016-04-18 23:51:21 +0200

    FadeIn to GameOver and Quit button
    duh

--- Scripts/TitleManager.cs
+++ Scripts/TitleManager.cs
@@ -5,24 +5,60 @@ using UnityEngine.SceneManagement;
 public class TitleManager : MonoBehaviour {
 
 	public CameraFade FadeManager;
-	public GameObject ArrowUp;
-	public GameObject ArrowDown;
+	public GameObject Arrow;
 
-	private bool isStartSelected = true;
+	public Vector3 StartPos;
+	public Vector3 InstructionsPos;
+	public Vector3 QuitPos;
+
 	private bool isAxisDown = false;
+
+	private int arrowPos = 0;
 	
 	// Update is called once per frame
 	void Update () 
 	{
 		if(Input.GetButtonDown("Fire1"))
 		{
-			FadeManager.FadeOut( isStartSelected? "Scene" : "Instructions" );
+			switch(arrowPos)
+			{
+			case 0:
+				FadeManager.FadeOut("Scene" );
+
+				break;
+			case 1:
+				FadeManager.FadeOut("Instructions" );
+
+				break;
+			case 2:
+				Application.Quit();
+				break;
+			}
+
 		}
 		else if(!isAxisDown && Input.GetAxisRaw("Vertical") != 0)
 		{
-			isStartSelected = !isStartSelected;
-			ArrowUp.SetActive( isStartSelected );
-			ArrowDown.SetActive( !isStartSelected );
+			if(Input.GetAxisRaw("Vertical") < 0 )
+			{
+				arrowPos = (arrowPos + 1) % 3;
+			}
+			else
+			{
+				arrowPos = (arrowPos == 0) ? 2 : arrowPos -1;
+			}
+
+			switch(arrowPos)
+			{
+			case 0:
+				Arrow.transform.position = StartPos;
+				break;
+			case 1:
+				Arrow.transform.position = InstructionsPos;
+				break;
+			case 2:
+				Arrow.transform.position = QuitPos;
+				break;
+			}
 		}
 		isAxisDown = Input.GetAxisRaw("Vertical") != 0;
 	}
