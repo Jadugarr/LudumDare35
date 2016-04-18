dcc78f3bff82555b4264cace1de4e2bf2c11fa6d
SirObvious <Pascal.Worreschk@gmail.com>
2016-04-18 21:15:43 +0200

    Point System + Game Over Restart System

--- PointSystem.cs
+++ PointSystem.cs
@@ -1,16 +1,21 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using AssemblyCSharp;
 
 public class PointSystem : MonoBehaviour 
 {
+
+    public Text pointText;
 	private EventManager eventManager = EventManager.Instance;
 	private MonsterPointsMap pointsMap = MonsterPointsMap.Instance;
-	private float points;
+	public static float points;
 	private float comboMultiplier;
 
 	void Awake()
 	{
+        this.pointText.text = "Score: 0";
+        this.comboMultiplier = 1;
 		AddEventListeners ();
 	}
 
@@ -24,21 +29,22 @@ public class PointSystem : MonoBehaviour
 	private void AddEventListeners()
 	{
 		eventManager.RegisterForEvent (EventTypes.MonsterKilled, OnMonsterKilled);
-		eventManager.RegisterForEvent (EventTypes.MonsterMissed, OnMonsterKilled);
+		eventManager.RegisterForEvent (EventTypes.MonsterMissed, OnMonsterMissed);
 	}
 
 	private void RemoveEventListeners()
 	{
 		eventManager.RemoveFromEvent (EventTypes.MonsterKilled, OnMonsterKilled);
-		eventManager.RemoveFromEvent (EventTypes.MonsterMissed, OnMonsterKilled);
+		eventManager.RemoveFromEvent (EventTypes.MonsterMissed, OnMonsterMissed);
 	}
 
 	private void OnMonsterKilled(IEvent evt)
 	{
 		MonsterKilledEvent evtArgs = (MonsterKilledEvent)evt;
 
-		this.points += this.pointsMap.GetPointsByMonsterType (evtArgs.KilledMonster.GetComponent<MonsterTypeComponent>().monsterType) * this.comboMultiplier;
+		PointSystem.points += this.pointsMap.GetPointsByMonsterType (evtArgs.KilledMonster.GetComponent<MonsterTypeComponent>().monsterType) * this.comboMultiplier;
 		this.comboMultiplier += 0.5f;
+        this.pointText.text = "Score: " + PointSystem.points;
 	}
 
 	private void OnMonsterMissed(IEvent evt)
---
+++ Scripts/ScoreScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreScript : MonoBehaviour {
+
+    public Text pointText;
+    private float timeAcc = 0f;
+
+	// Use this for initialization
+	void Start () {
+        pointText.text = PointSystem.points.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeAcc += Time.deltaTime;
+        if(timeAcc > 2f)
+        {
+            if (Input.anyKeyDown)
+            {
+                SceneManager.LoadScene("Title");
+            }
+        }
+	}
+}
