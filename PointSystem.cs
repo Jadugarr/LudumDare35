4337effa396694a0ac43156ffc29394ac453ce28
Jadugarr <ReneLoescher@gmx.net>
2016-04-18 20:26:53 +0200

    Implemented point system

---
+++ PointSystem.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using AssemblyCSharp;
+
+public class PointSystem : MonoBehaviour 
+{
+	private EventManager eventManager = EventManager.Instance;
+	private MonsterPointsMap pointsMap = MonsterPointsMap.Instance;
+	private float points;
+	private float comboMultiplier;
+
+	void Awake()
+	{
+		AddEventListeners ();
+	}
+
+	void OnDestroy()
+	{
+		RemoveEventListeners ();
+	}
+
+	public PointSystem(){}
+
+	private void AddEventListeners()
+	{
+		eventManager.RegisterForEvent (EventTypes.MonsterKilled, OnMonsterKilled);
+		eventManager.RegisterForEvent (EventTypes.MonsterMissed, OnMonsterKilled);
+	}
+
+	private void RemoveEventListeners()
+	{
+		eventManager.RemoveFromEvent (EventTypes.MonsterKilled, OnMonsterKilled);
+		eventManager.RemoveFromEvent (EventTypes.MonsterMissed, OnMonsterKilled);
+	}
+
+	private void OnMonsterKilled(IEvent evt)
+	{
+		MonsterKilledEvent evtArgs = (MonsterKilledEvent)evt;
+
+		this.points += this.pointsMap.GetPointsByMonsterType (evtArgs.KilledMonster.GetComponent<MonsterTypeComponent>().monsterType) * this.comboMultiplier;
+		this.comboMultiplier += 0.5f;
+	}
+
+	private void OnMonsterMissed(IEvent evt)
+	{
+		this.comboMultiplier = 1f;
+	}
+}
---
+++ Scripts/Models/MonsterPointsMap.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AssemblyCSharp
+{
+	public class MonsterPointsMap
+	{
+		private static MonsterPointsMap instance = new MonsterPointsMap();
+		public static MonsterPointsMap Instance
+		{
+			get
+			{
+				return MonsterPointsMap.instance;
+			}
+		}
+
+		private Dictionary<MonsterType, int> pointMap;
+
+		private MonsterPointsMap ()
+		{
+			this.pointMap = new Dictionary<MonsterType, int> () {
+				{ MonsterType.HarveyeEyetel, 50 },
+				{ MonsterType.Hoarsethrot, 50 },
+				{ MonsterType.JohnWig, 50 },
+				{ MonsterType.LeviNomster, 50 },
+				{ MonsterType.M, 50 },
+				{ MonsterType.Mandible, 50 },
+				{ MonsterType.MM, 50 },
+				{ MonsterType.MMMs, 50 },
+				{ MonsterType.NottaReeskin, 50 },
+				{ MonsterType.ScibbLegdai, 50 },
+				{ MonsterType.Weeknees, 50 }
+			};
+		}
+
+		public int GetPointsByMonsterType(MonsterType monsterType)
+		{
+			return this.pointMap [monsterType];
+		}
+	}
+}
+
